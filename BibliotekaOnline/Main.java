0c8062f76dff8ff6288fd6a63aab5a5b613cff14
GG-RK <grzes.grzeskow@gmail.com>
2020-05-23 17:28:25 +0200

    ServerConnection alpha

---
+++ BibliotekaOnline/Main.java
@@ -0,0 +1,20 @@
+import java.security.Policy;
+import java.sql.DriverManager;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        ServerConnection s=new ServerConnection("jdbc:derby:Biblioteka_Online");
+        //s.CreateEmpty();
+        s.Connect();
+        s.Register("user1", "email@ksiazki.pl","haslo1");
+        s.Register("user2", "tak@ksiazki.pl","haslo2");
+        ArrayList<String> users=s.GetUsers();
+
+        for(int i=0;i<users.size();i++) System.out.println(users.get(i));
+
+        s.Disconnect();
+    }
+}
